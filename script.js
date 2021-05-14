09626693862dc81898cab0c3e3e96c452a8e4bf7
Carolina Reedy <carinvid@gmail.com>
2021-05-14 16:12:16 -0600

    modifying javascript

--- script.js
+++ script.js
@@ -1,40 +1,114 @@
+var generateBtn = document.querySelector("#generate");
 
-window.prompt("How many characters would you like your password contain?")
-if (passwordLength < 8 || passwordLength > 129) {
-  window.alert("cannot be less than 8 characters or more than 129 ")
-  window.prompt("Please try again");
-  console.log(passwordLength);
- };
- window.alert(" Click OK to corfirm including numeric characters.")
- window.alert(" Click OK to corfirm including upper cases.")
- window.alert(" Click OK to corfirm including lower characters.")
+function generatePassword() {
+  var uppercaseList = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  var lowercaseList = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  var numbericList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var characterList = [
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "-",
+    "_",
+    "=",
+    "+",
+  ];
 
-function generate() {
-var upperase = "ABCDEFGHIJKLMNOPQRSTUYVWXZ";
-var lowerCase = "abcdefghijklmnopqrstuvwyxz";
-var numbers = "0123456789";
-var sysmbols = "!@#$%^&*(){}[]:";
-charSet.passwordLength;
-var password = '';
-for (var i =7; i < passwordLength, i ++) {
-  var random = Math.floor(Math.random() * charSet.passwordLength);
-  password += charSet.substring (random, random + 1);
+  // empty array
+
+  var resultArray = [];
+  var userArray = [];
+
+  uppercaseArray[1];
+  //-----------------------------------------------------------------
+  var numCharacter = prompt(
+    "How much number of Character you want between 8 and 128?"
+  );
+  var numbers = confirm("you want numbers in your password?");
+  var uppercases = confirm("you want Uppercases in your password?");
+  var lowercases = confirm("you want lowercases in your password?");
+  var characters = confirm("you want special characters in your password?");
+
+  if (numbers) {
+    resultArray = resultArray.concat(numbArray);
+  }
+
+  if (uppercases) {
+    resultArray = resultArray.concat(uppercaseArray);
+  }
+
+  if (lowercases) {
+    resultArray = resultArray.concat(lowercaseArray);
+  }
+
+  if (characters) {
+    resultArray = resultArray.concat(characterArray);
+  }
+  console.log(resultArray);
+
+  for (var i = 0; i < numCharacter; i++) {
+    userArray.push(resultArray[Math.floor(Math.random() * resultArray.length)]);
+  }
+
+  return userArray.join("");
 }
-document.getElementById().value = password;
-
-
-
-// Assignment Code
-var generate = document.querySelector("#generate");
-
-// Write password to the #password input
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", generate);
